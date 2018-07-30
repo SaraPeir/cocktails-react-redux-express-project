94ae8f0366bfe014b71304073f43ad0915b09ac6
Sara Peirce <sarpeirce@gmail.com>
2018-07-30 21:11:54 +0200

    recipes scheme

--- client/src/components/Recipes/Form.js
+++ client/src/components/Recipes/Form.js
@@ -1,104 +1,106 @@
 import React from 'react';
 import { Component } from 'react';
-import { connect } from 'react-redux'
-//import { changeSelectText } from '../../actions';
 
-
-class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state ={
-          name: '',
-          ingredients: '',
-          recipe: '',
-          data: []
-        }
-    
-        this.writeName = this.writeName.bind(this);
-        this.writeIngredients = this.writeIngredients.bind(this);
-        this.writeRecipe = this.writeRecipe.bind(this);
-        this.updateData = this.updateData.bind(this);
-      }
-    
-      writeName(event){
-        this.setState({
-          name: event.target.value
-        });
-      }
-
-      writeIngredients(event){
-        this.setState({
-          ingredients: event.target.value
-        });
-      }
-
-      writeRecipe(event){
-        this.setState({
-          recipe: event.target.value
-        });
-      }
-
-      updateData(){
-        const {name, ingredients, recipe, data} = this.state;
-        const newName = name;
-        const newIngredients = ingredients;
-        const newRecipe = recipe;
-        if(newName !== '' && newIngredients !== '' && newRecipe !== '') {
-            this.setState({
-                data: data.concat({newName, newIngredients, newRecipe})
-            });
-        }
-        
-        console.log('data', this.state.data);
-        
-        this.resetName();
-        this.resetIngredients();
-        this.resetRecipe();
-      }
-
-      resetName(){
-        this.setState({
-            name: ''
-          });
-      }
-
-      resetIngredients(){
-        this.setState({
-            ingredients: ''
-          });
-      }
-
-      resetRecipe(){
-        this.setState({
-            recipe: ''
-          });
-      }
- 
+class Form extends Component { 
   render() {
     return (
       <div>
-      <input type="text" placeholder="Name" onChange={this.writeName} value={this.state.name} />
-      <textarea rows="4" cols="50" placeholder="Ingredients" onChange={this.writeIngredients} value={this.state.ingredients}></textarea>
-      <textarea rows="4" cols="50" placeholder="Recipe" onChange={this.writeRecipe} value={this.state.recipe}></textarea>
-      <button onClick={this.updateData}>Ok</button>
+      <input type="text" placeholder="Name" onChange={this.props.onWriteName} value={this.props.cocktailName} />
+      <textarea rows="4" cols="50" placeholder="Ingredients" onChange={this.props.onWriteIngredients} value={this.props.cocktailIngredients}></textarea>
+      <textarea rows="4" cols="50" placeholder="Recipe" onChange={this.props.onWriteRecipe} value={this.props.cocktailRecipe}></textarea>
+      <button onClick={this.props.onUpdateData}>Ok</button>
       </div>
     );
   }
 }
 
-// function mapStateToProps(state) {
-//   return {
-//     arrayList: state.dataNoAlc.drinksDataNoAlc,
-//     selectedOption: state.selVal.selectValue
-//   }
-
-// }
-
 export default Form;
 
-// export default connect(mapStateToProps, { changeSelectText })(Form);
 
-// <select value = {this.props.selectedOption} onChange = {this.props.changeSelectText}><option>Analcolic drinks</option>{this.createOptions()}</select>
+// import React from 'react';
+// import { Component } from 'react';
 
+// class Form extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state ={
+//           name: '',
+//           ingredients: '',
+//           recipe: '',
+//           data: []
+//         }
+    
+//         this.writeName = this.writeName.bind(this);
+//         this.writeIngredients = this.writeIngredients.bind(this);
+//         this.writeRecipe = this.writeRecipe.bind(this);
+//         this.updateData = this.updateData.bind(this);
+//       }
+    
+//       writeName(event){
+//         this.setState({
+//           name: event.target.value
+//         });
+//       }
 
+//       writeIngredients(event){
+//         this.setState({
+//           ingredients: event.target.value
+//         });
+//       }
+
+//       writeRecipe(event){
+//         this.setState({
+//           recipe: event.target.value
+//         });
+//       }
+
+//       updateData(){
+//         const {name, ingredients, recipe, data} = this.state;
+//         const newName = name;
+//         const newIngredients = ingredients;
+//         const newRecipe = recipe;
+//         if(newName !== '' && newIngredients !== '' && newRecipe !== '') {
+//             this.setState({
+//                 data: data.concat({newName, newIngredients, newRecipe})
+//             });
+//         }
+        
+//         console.log('data', this.state.data);
+        
+//         this.resetName();
+//         this.resetIngredients();
+//         this.resetRecipe();
+//       }
+
+//       resetName(){
+//         this.setState({
+//             name: ''
+//           });
+//       }
+
+//       resetIngredients(){
+//         this.setState({
+//             ingredients: ''
+//           });
+//       }
+
+//       resetRecipe(){
+//         this.setState({
+//             recipe: ''
+//           });
+//       }
+ 
+//   render() {
+//     return (
+//       <div>
+//       <input type="text" placeholder="Name" onChange={this.writeName} value={this.state.name} />
+//       <textarea rows="4" cols="50" placeholder="Ingredients" onChange={this.writeIngredients} value={this.state.ingredients}></textarea>
+//       <textarea rows="4" cols="50" placeholder="Recipe" onChange={this.writeRecipe} value={this.state.recipe}></textarea>
+//       <button onClick={this.updateData}>Ok</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Form;
 
--- client/src/components/Recipes/RecipesPage.js
+++ client/src/components/Recipes/RecipesPage.js
@@ -5,15 +5,91 @@ import Form from './Form';
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
 class RecipesPage extends Component {
+        constructor(props) {
+        super(props);
+        this.state = {
+          name: '',
+          ingredients: '',
+          recipe: '',
+          data: []
+        }
+    
+        this.writeName = this.writeName.bind(this);
+        this.writeIngredients = this.writeIngredients.bind(this);
+        this.writeRecipe = this.writeRecipe.bind(this);
+        this.updateData = this.updateData.bind(this);
+      }
+    
+      writeName(event){
+        this.setState({
+          name: event.target.value
+        });
+      }
+
+      writeIngredients(event){
+        this.setState({
+          ingredients: event.target.value
+        });
+      }
+
+      writeRecipe(event){
+        this.setState({
+          recipe: event.target.value
+        });
+      }
+
+      updateData(){
+        const {name, ingredients, recipe, data} = this.state;
+        const newName = name;
+        const newIngredients = ingredients;
+        const newRecipe = recipe;
+        if(newName !== '' && newIngredients !== '' && newRecipe !== '') {
+            this.setState({
+                data: data.concat({newName, newIngredients, newRecipe})
+            });
+        }
+        
+        console.log('data', this.state.data);
+        
+        this.resetName();
+        this.resetIngredients();
+        this.resetRecipe();
+      }
+
+      resetName(){
+        this.setState({
+            name: ''
+          });
+      }
+
+      resetIngredients(){
+        this.setState({
+            ingredients: ''
+          });
+      }
+
+      resetRecipe(){
+        this.setState({
+            recipe: ''
+          });
+      }
 
   render() {
+    const {name, ingredients, recipe, data} = this.state;
     return (
       <div>
         <Link to={`/`} style={{ textDecoration: 'none', color: 'black', textAlign: 'center'}}><button>Home</button></Link>
-        <Form />
+        <Form cocktailName = {name} cocktailIngredients = {ingredients} cocktailRecipe = {recipe} 
+            onWriteName = {this.writeName} onWriteIngredients = {this.writeIngredients } onWriteRecipe = {this.writeRecipe} 
+            onUpdateData = {this.updateData} />
       </div>
     );
   }
 }
 
 export default RecipesPage;
+
+
+//en FOrm, al rededor del button, pone un link que lleve a la pantalla entera solo de recetas WholeRecipeSecgtion (v. mood calendar)
+// con el array que se forma aquí, hay que hacer un map de tarjetas que cadauna es una receta.
+//
